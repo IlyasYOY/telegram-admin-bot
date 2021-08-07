722aaffca3e9e64834b9df15d9128ba68e8c3802
Ильиных Илья Сергеевич <ilyasyoy@gmail.com>
2021-08-07 22:24:03 +0300

    migrate to spring boot

---
+++ src/main/java/ru/ilyasyoy/telegram/admin/TelegramAdminBotApplication.java
@@ -0,0 +1,13 @@
+package ru.ilyasyoy.telegram.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramAdminBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramAdminBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ilyasyoy/telegram/admin/TelegramAdminBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilyasyoy.telegram.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramAdminBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
